f55c0c45861d9d8677d5755c213313ce3b07172b
Laetitia-ghb <kevlaetice8@gmail.com>
2021-11-18 15:53:25 +0100

    made changes

--- main.dart
+++ main.dart
@@ -1,8 +1,40 @@
 void main() {
-  //  finding the Sum of 2 integers();
-  print(sum(80,20));
+ // Working with variables
+ int birthyear = 2000;
+ int currentyear = 2021;
+ int (age) = currentyear - birthyear;
+ print (age);
+ }
+
+ int age = 20;
+ if (age>= 18) {
+   print('you are and adult');
+   }
+   //If else statement
+   int age2 = 20;
+   if (age2 >= 18) {
+     print('you are an adult');
+     } else {
+Print ('you are not an adult');
 }
 
-int sum(int firstNum, int secondNum) {
-  return firstNum + secondNum;
+//Nested if else
+int marks = 70;
+if (marks < 40) {
+  print ('chairman you failed oo');
+  } else if (marks <= 50) {
+    print ('chale you nearly failed');
+    } else if (marks <= 75) {
+      print ('you are doing well');
+      } else {
+        print ('Well done');
 }
+
+//Switch statements
+
+//Functions
+void sayHello() {
+  print('Hello');
+}
+
+sayHello();
